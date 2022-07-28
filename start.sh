bdf5136955459e7ed13aa90e9cc61d038f5e3192
Zach Fleeman <zfleeman@protonmail.com>
2022-07-28 17:21:28 -0400

    Update start script

--- start.sh
+++ start.sh
@@ -7,17 +7,15 @@ pip3 install --user -r requirements.txt > /dev/null 2>&1
 
 echo "Starting backend..."
 cd backend/app
-uvicorn main:app > /dev/null 2>&1 &
+python -m uvicorn main:app > /dev/null 2>&1 &
 BPID=$!
 echo $BPID > ../../trailhead_backend.pid
 cd ../..
 
-
-
 echo "Starting frontend..."
 cd frontend
-yarn --silent > /dev/null 2>&1
-yarn --silent serve > /dev/null 2>&1 &
+npm install > /dev/null 2>&1
+npm run serve > /dev/null 2>&1 &
 FPID=$!
 echo $FPID > ../trailhead_frontend.pid
 cd ..
